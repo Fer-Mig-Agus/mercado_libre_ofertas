7d142a0d7bcf5d55caec7f449f291d5e1bafb971
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2025-02-22 22:49:28 -0300

    Correciones realizadas, se espera el proyecto finalizado para la nueva fecha de entrega

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+'''
+
+NO SE CUMPLIO CON LO PEDIDO
+
+El crawler no extare nada de informacion, los archivos se crean sin que existan datos.
+Leer atentamente todas las anotaciones y corregir el codigo.
+
+Fecha para la nueva entrega: 25/02/25 a las 23:55
+
+Si para la fecha no se concreta el proyecto,
+se dara como proyecto insatisfecho y se mostrara una manera de realizar el mismo
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+
+'''
+Esta importacion esta de mas ya que no hace falta importar el openpyxl
+
+'''
 from openpyxl import Workbook
 
 # URL
@@ -13,7 +44,9 @@ headers = {
 # Lista para almacenar los datos
 data = []
 
-
+'''
+Hacer uso del try exception en caso de ser necesario.
+'''
 # Función para extraer información de una página
 def scrape_page(url):
     response = requests.get(url, headers=headers)
@@ -45,9 +78,22 @@ def scrape_page(url):
 
 # Realizar el scrapeo de las 10 páginas
 for page in range(1, 11):
+    # OJO
+    '''
+    Tener en cuenta la URL BASE para la pagina 1 es esta:
+    https://listado.mercadolibre.com.ar/_Container_mk-pps-liquidacion-verano-2025-audio_NoIndex_True
+    Y para las demas paginas es esta:
+    https://listado.mercadolibre.com.ar/_Desde_49_Container_mk-pps-liquidacion-verano-2025-audio_NoIndex_True
+    
+    Aqui la forma en la que re defines la url esta mal: OJO con la estructura de la URL
+    '''
     url = f"{base_url}_Desde_{(page - 1) * 50 + 1}"
     scrape_page(url)
 
+'''
+Revisar que la variable data tenga elementos para poder crear los archivos
+Sino no tiene sentido crear si no hay datos
+'''
 # Crear un DataFrame con los datos
 df = pd.DataFrame(data)
 
@@ -55,7 +101,29 @@ df = pd.DataFrame(data)
 df.to_csv('mercado_libre_scraping.csv', index=False)
 print("Datos guardados en 'mercado_libre_scraping.csv'")
 
+
+'''
+Aqui puedes aclarar el motor para formar el archivo excel, recuerda es el openpyxl
+Ejemplo:
+
+df.to_excel('mercado_libre_scraping.xlsx', index=False, engine='openpyxl')
+'''
 # Guardar los datos en un archivo XLSX
 df.to_excel('mercado_libre_scraping.xlsx', index=False)
 print("Datos guardados en 'mercado_libre_scraping.xlsx'")
+
+
+'''
+Usar la estrutura de if name == main
+Para no tener problemas con la ejecucion del programa
+
+
+if __name__ == '__main__':
+    #Ejecuto el programa
+
+Averiguar para que sirve esto.
+
+Leer el archivo de requirements. Corregir errores
+'''
+
 
